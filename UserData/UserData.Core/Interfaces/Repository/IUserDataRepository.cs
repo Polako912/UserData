56472cbf3d5789e05e42aeb53d046f176acc6e58
Michał Polak <235046@student.pwr.edu.pl>
2020-06-02 19:06:39 +0200

    added user model and interface for repo

---
+++ UserData/UserData.Core/Interfaces/Repository/IUserDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserData.Core.Interfaces.Repository
+{
+    public interface IUserDataRepository
+    {
+        Task<User> Get(int id);
+        Task Add(User user);
+        Task Modify(User user);
+        Task Delete(User user);
+    }
+}
---
+++ UserData/UserData.Core/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserData
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Street { get; set; }
+        public int StreetNumber { get; set; }
+    }
+}
