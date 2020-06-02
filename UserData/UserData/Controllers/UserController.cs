0c9b15383b5d248eeebd7172157ca832823ddbf0
Michał Polak <235046@student.pwr.edu.pl>
2020-06-02 19:18:16 +0200

    added controllers

---
+++ UserData/UserData/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserData.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("{number}")]
+        public async Task<IActionResult> Get([FromRoute] int number)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] User user)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async  Task<IActionResult> Delete([FromBody] User user)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Modify([FromBody] User user)
+        {
+            return Ok();
+        }
+    }
+}
